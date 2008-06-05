bd734a51650ecdcbf2ec0a08944c434b5607bcc4
Nils Homer <nhomer@cs.ucla.edu>
2008-06-05 20:02:53 +0000

    Added a stat module for indexes

---
+++ butil/bindexstat.c
@@ -0,0 +1,97 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+
+#include "../blib/BLibDefinitions.h"
+#include "../blib/BError.h"
+#include "../blib/RGIndex.h"
+#include "bindexstat.h"
+
+int main(int argc, char *argv[]) 
+{
+	FILE *fp=NULL;
+	char indexFileName[MAX_FILENAME_LENGTH]="\0";
+	char rgFileName[MAX_FILENAME_LENGTH]="\0";
+
+	if(argc == 3) {
+		RGBinary rg;
+		RGIndex index;
+
+		strcpy(rgFileName, argv[1]);
+		strcpy(indexFileName, argv[2]);
+
+		/* Read in the rg binary file */
+		RGBinaryReadBinary(&rg, rgFileName);
+
+		/* Read the index */
+		fprintf(stderr, "Reading in index from %s.\n",
+				indexFileName);
+		if(!(fp=fopen(indexFileName, "rb"))) {
+			PrintError("bfixhash",
+					indexFileName,
+					"Could not open file for reading",
+					Exit,
+					OpenFileError);
+		}
+		RGIndexRead(fp, &index, 1);
+		fclose(fp);
+
+		/* Get the desired stats */
+		PrintMeanAndVarianceOfCAL(&index, &rg);
+
+		fprintf(stderr, "%s", BREAK_LINE);
+		fprintf(stderr, "Cleaning up.\n");
+		/* Delete the index */
+		RGIndexDelete(&index);
+		/* Delete the rg */
+		RGBinaryDelete(&rg);
+	}
+	else {
+		fprintf(stdout, "Please give a reference genome file name then an index file name.  Terminating!\n");
+	}
+
+	return 0;
+}
+
+void PrintMeanAndVarianceOfCAL(RGIndex *index, RGBinary *rg)
+{
+	int64_t start, end;
+	int64_t numEntries;
+	long double sum;
+	long double mean, variance;
+
+	/* Get the mean */
+	numEntries = 0;
+	start = 0;
+	for(end=1;end < index->length;end++) {
+		int cmp = RGIndexCompareAt(index, rg, start, end);
+		assert(cmp <= 0);
+		if(cmp < 0) {
+			numEntries++;
+			/* Update */
+			start = end;
+		}
+	}
+	mean = (index->length*1.0)/numEntries;
+
+	/* Get the variance */
+	sum = 0.0;
+	start = 0;
+	for(end=1;end < index->length;end++) {
+		int cmp = RGIndexCompareAt(index, rg, start, end);
+		assert(cmp <= 0);
+		if(cmp < 0) {
+			int val = ( (end-1) - start + 1);
+			sum += (val - mean)*(val - mean);
+			/* Update */
+			start = end;
+		}
+	}
+	variance = sum/numEntries;
+
+	fprintf(stderr, "The mean was: %Lf\nThe variance was: %Lf\n",
+			mean,
+			variance);
+}
+
---
+++ butil/bindexstat.h
@@ -0,0 +1,9 @@
+#ifndef BINDEXSTAT_H_
+#define BINDEXSTAT_H_
+
+#include "../blib/RGIndex.h"
+#include "../blib/RGBinary.h"
+
+void PrintMeanAndVarianceOfCAL(RGIndex*, RGBinary*);
+
+#endif
