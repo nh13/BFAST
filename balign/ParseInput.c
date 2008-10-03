b7e5876f954dd6b541cf8bd4811a56f7cd9fc8ea
Nils Homer <nhomer@cs.ucla.edu>
2008-10-03 21:21:21 +0000

    Updated trunk to 0.1.4

--- balign/ParseInput.c
+++ balign/ParseInput.c
@@ -44,7 +44,7 @@
 #include "ParseInput.h"
 
 const char *argp_program_version =
-"balign version 0.1.3\n"
+"balign version 0.1.4\n"
 "Copyright 2008.";
 
 const char *argp_program_bug_address =
--- bmatches/ParseInput.c
+++ bmatches/ParseInput.c
@@ -44,7 +44,7 @@
 #include "ParseInput.h"
 
 const char *argp_program_version =
-"bmatches version 0.1.3\n"
+"bmatches version 0.1.4\n"
 "Copyright 2008.";
 
 const char *argp_program_bug_address =
