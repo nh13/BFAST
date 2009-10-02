4bd02a93fab545b8a2c53704f5180ac841aa9671
nhomer <nhomer@rocks5.genome.ucla.edu>
2009-10-02 00:29:38 -0700

    The test cleanup script will not complain if it could not clean everything up.

--- tests/test.cleanup.sh
+++ tests/test.cleanup.sh
@@ -4,13 +4,8 @@
 
 echo "      Cleaning up files.";
 
-rm -r $OUTPUT_DIR $TMP_DIR;
-ls -1 $DATA_DIR/* | grep -v bz2 | xargs rm;
-
-# Get return code
-if [ "$?" -ne "0" ]; then
-	exit 1
-fi
+rm -r $OUTPUT_DIR $TMP_DIR 2> /dev/null;
+ls -1 $DATA_DIR/* | grep -v bz2 | xargs rm 2> /dev/null;
 
 # Test passed!
 echo "      Files cleaned up.";
