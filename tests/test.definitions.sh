a9bbb8fb5bb935735154c0f16539ecaa76388063
Nils Homer <nhomer@cs.ucla.edu>
2009-12-26 22:38:02 -0700

    Updating ChangeLog to include SAM fields inclusion and updating tests.

--- tests/test.definitions.sh
+++ tests/test.definitions.sh
@@ -2,8 +2,8 @@
 
 # Stores all the definitions required for the tests
 
-#CMD_PREFIX="../bfast/";
-CMD_PREFIX="valgrind --leak-check=yes --log-file=pipe.valgrind ../bfast/";
+CMD_PREFIX="../bfast/";
+#CMD_PREFIX="valgrind --leak-check=yes --log-file=pipe.valgrind ../bfast/";
 REF_ID_CC="corner.cases";
 OUTPUT_ID_NT="phi.x.174";
 OUTPUT_ID_CC_NT="corner.cases.nt";
