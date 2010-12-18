2fc8e0b700707adbc49fa3bc11a4fa476bab4bf2
Kevin Squire <kevin.squire@gmail.com>
2010-12-17 16:56:17 -0800

    psolid2fastq.pl: * Modified script to submit array jobs * Fixed bug when splitting with small (<100,000) read sizes * Added --merge-single to allow merging of .single.fastq files   when splitting in bwa or single modes

--- scripts/psolid2fastq.pl
+++ scripts/psolid2fastq.pl
@@ -16,6 +16,7 @@ use POSIX qw/ceil/;
 use Fcntl ":seek";
 use Cwd 'abs_path';
 use Time::HiRes qw( gettimeofday tv_interval );
+use List::Util qw( min );
 
 my $path = dirname(abs_path($0));
 #my $extract_region = "$path/extract_region";
@@ -34,6 +35,9 @@ my $output_prefix;
 my $readname_prefix;
 my $basename;
 my $wait = 0;
+my $merge_single = 0;
+
+my $output_dir;
 
 GetOptions('help' => \$help,
            man => \$man,
@@ -45,16 +49,115 @@ GetOptions('help' => \$help,
            'wait' => \$wait,
 	   'b' => \$bwa_output,
 	   'w' => \$single_output,
-	   'Z' => \$gzip_output) or pod2usage(2);
+	   'Z' => \$gzip_output,
+	   'merge-single' => \$merge_single) or pod2usage(2);
 pod2usage(1) if $help;
 pod2usage(-exitstatus => 0, -verbose =>2) if $man;
 pod2usage("Please specify exactly one of --split_size or --chunks.") if ((!defined($split_size) && !defined($chunks)) || ($split_size && $chunks));
 pod2usage("Wrong number of arguments.") if ($#ARGV != 1 && $#ARGV != 3);
 pod2usage("Please specify at most one of -b or -w.") if ($bwa_output && $single_output);
+pod2usage("--merge-single is only useful for -b or -w modes.") if ($merge_single && !$bwa_output && !$single_output);
 
 my $FAKEQSUBID = 0;
 
 ################################################
+
+sub SubmitArrayJob {
+   my ($run_file, $job_file, $dryrun, $commands, $outputID, $threads, $dependent_job_ids, $sync) = @_;
+
+   print STDERR "[psolid2fastq] RUNFILE=$run_file  JOBFILE=$job_file\n";
+
+   open(FH, ">$job_file") or die("Error.  Could not open $job_file for writing!\n");
+
+   # create jobs file
+
+   my $job_num = 0;
+   for my $command (@$commands) {
+      $job_num++;
+      print FH "############# Job $job_num start #############\n";
+      print FH "$command\n";
+      print FH "############# Job $job_num end #############\n";
+   }
+   close(FH);
+
+   # Create script file
+
+   my $output = <<'END_OUTPUT';
+run ()
+{
+    echo \"running: $*\" 2>&1;
+    eval $*;
+    if test $? != 0 ; then
+        echo "error: while running '$*'";
+        exit 100;
+    fi
+}
+
+echo "==========="
+echo "Job $SGE_TASK_ID"
+echo "==========="
+run "date";
+run "hostname";
+lines=`awk -v job_start="Job $SGE_TASK_ID start" -v job_end="Job $SGE_TASK_ID end" \
+'$0 ~ job_start {
+    while (getline > 0 && $0 !~ job_end) print;
+    exit;
+}' $job_file`
+
+while read -r line
+do
+    run "$line";
+done <<< "$lines"
+
+exit 0;
+END_OUTPUT
+
+   $output =~ s/\$job_file/$job_file/g;
+
+   open(FH, ">$run_file") or die("Error.  Could not open $run_file for writing!\n");
+   print FH "$output";
+   close(FH);
+
+   # Create qsub command
+   my $qsub = "qsub -terse";
+
+   $qsub .= " -t 1-$job_num";
+
+   if (1 < $threads) {
+      $qsub .= " -pe serial ".$threads;
+   }
+
+   if(0 < scalar(@$dependent_job_ids)) {
+       $qsub .= " -hold_jid ".join(",", @$dependent_job_ids);
+   }
+
+   if (0 < $sync) {
+      $qsub .= " -sync y";
+   }
+
+   $qsub .= " -N $outputID -o $run_file.\$TASK_ID.out -e $run_file.\$TASK_ID.err $run_file";
+
+   if(1 == $dryrun) {
+      $FAKEQSUBID++;
+      print $qsub."\n";
+      print STDERR "[psolid2fastq] NAME=$outputID QSUBID=$FAKEQSUBID\n";
+      return $FAKEQSUBID;
+   }
+
+   my $qsub_id = `$qsub`;
+   $qsub_id = "$qsub_id";
+   chomp($qsub_id);
+
+   die("Error submitting QSUB_COMMAND=$qsub\nQSUB_ID=$qsub_id\n") unless (0 < length($qsub_id));
+   if($qsub_id !~ m/^\S+$/) {
+      die("Error submitting QSUB_COMMAND=$qsub\nQSUB_ID=$qsub_id\n") unless (0 < length($qsub_id));
+   }
+
+   print STDERR "[psolid2fastq] NAME=$outputID QSUBID=$qsub_id\n";
+
+   return $qsub_id;
+
+}
 
 sub SubmitJob {
 
@@ -172,6 +275,10 @@ sub get_next_read {
    my $line;
    my $pos;
 
+   if (eof($FILE)) {
+      return -1;
+   }
+
    for ($i = 0; $i < 3; $i++) {
       $pos = tell($FILE);
       $line = <$FILE>;
@@ -185,27 +292,33 @@ sub get_next_read {
 }
 
 sub get_matching_read {
-   my ($FILE, $readname, $qual_seek_pos, $step_size) = @_;
+   my ($FILE, $readname, $new_readname, $seek_pos, $step_size, $file_size, $exact) = @_;
    my $pos;
-   my $qual_readname;
 
-   my $last = $#$qual_seek_pos;
+   my $last = $#$seek_pos;
 
-   my $step_back = - $step_size/250;
+   # Determined emperically.  If the estimated position is overshot,
+   # most reads require only one step back.
+   my $step_back = - min($step_size/256, 4096);
 
-   if (@$qual_seek_pos < 2) {
-       $pos = $qual_seek_pos->[$last] + $step_size;
-   }
-   elsif (@$qual_seek_pos == 2) {
-       $pos = $qual_seek_pos->[1] * 2 - $qual_seek_pos->[0];
+   if (@$seek_pos < 3) {
+       $pos = $seek_pos->[$last] + $step_size;
    }
    else {
+       # Estimate next position based on last two positions and
+       # assuming a quadratic curve.  Empirically, it works better 
+       # than not doing it, but it's not perfect.
        my ($a, $b, $c);
-       $a = $qual_seek_pos->[$last-1];
-       $c = ($qual_seek_pos->[$last-2] + $qual_seek_pos->[$last])/2 - $a;
-       $b = $a + $c - $qual_seek_pos->[$last-2];
+       $a = $seek_pos->[$last-1];
+       $c = ($seek_pos->[$last-2] + $seek_pos->[$last])/2 - $a;
+       $b = $a + $c - $seek_pos->[$last-2];
 
        $pos = $a + 2*$b + 4*$c;
+   }
+
+   # To protect against craziness near the end of a file...
+   if ($pos > $file_size + $step_back) {
+       $pos = $file_size + $step_back;
    }
 
    seek($FILE, $pos, 0);
@@ -220,27 +333,29 @@ sub get_matching_read {
 	 }
       }
 
-      $qual_readname = decode($_);
+      $$new_readname = decode($_);
+      $pos = tell($FILE) - length;
 
-      if($qual_readname > $readname) {
+      if($$new_readname > $readname) {
 	 seek($FILE, $step_back, 1);
       }
+      else {
+	 $pos = tell($FILE) - length;
+      }
+   } until ($$new_readname <= $readname);
 
-   } until ($qual_readname <= $readname);
-
-   #print STDERR "Readname: $readname   Qual readname: $qual_readname\r";
+   if (0 == $exact) {
+      return $pos;
+   }
 
    do {
       if (m/^>/) {
-	 $qual_readname = decode($_);
-	 #print STDERR "Readname: $readname   Qual readname: $qual_readname\r";
-      }
+	 $$new_readname = decode($_);
+	 if ($$new_readname == $readname) {
+	    $pos = tell($FILE) - length;
 
-      if (m/^>/ && decode($_) == $readname) {
-         $pos = tell($FILE) - length;
-	 #print STDERR "Qual file pos: $pos                                                           \n";
-
-         return $pos;
+	    return $pos;
+	 }
       }
    } while (<$FILE>);
 
@@ -309,11 +424,12 @@ my $input_dir = rel2abs(dirname($csfasta_files[0]));
 if (!defined($output_prefix)) {
    $basename = fileparse($csfasta_files[0], "_F[35].*\.csfasta");
    $output_prefix = $input_dir."/".$basename;
+   $output_dir = rel2abs(dirname($output_prefix));
 }
 else {
    $output_prefix = rel2abs($output_prefix);
    $basename = fileparse($output_prefix);
-   my $output_dir = dirname($output_prefix);
+   $output_dir = rel2abs(dirname($output_prefix));
    if (!-d$output_dir) {
       mkpath($output_dir);
    }
@@ -348,28 +464,35 @@ for ($chunk = 1; $chunk < $chunks; $chunk++) {
    my $read_name_1;
    my $read_name_2;
 
-   seek ($csfasta_fhs[0], $chunk * $csfasta_chunk_sizes[0], 0);   
+   my $qual_read_name_1;
+   my $qual_read_name_2;
+
+   seek ($csfasta_fhs[0], $chunk * $csfasta_chunk_sizes[0], 0);
    $csfasta_pos[0] = get_next_read($csfasta_fhs[0], \$read_name_1);
 
    if ($paired) {
-      seek ($csfasta_fhs[1], $chunk * $csfasta_chunk_sizes[1], 0);
-      $csfasta_pos[1] = get_next_read($csfasta_fhs[1], \$read_name_2);
+      $csfasta_pos[1] = get_matching_read($csfasta_fhs[1], $read_name_1, \$read_name_2,
+					  $csfasta_seek_pos[1], $csfasta_chunk_sizes[1],
+					  $csfasta_file_sizes[1], 0);
 
       do {
          # find read_name_2 >= read_name_1 in csfasta_2
-	 #print "Read name 1: $read_name_1\n";
          while($read_name_2 < $read_name_1) {
             $csfasta_pos[1] = get_next_read($csfasta_fhs[1], \$read_name_2);
-	    #print "Read name 2: $read_name_2\r";
+	    if ($csfasta_pos[1] == -1) {
+	       print STDERR "Warning! Reached end of $csfasta_files[1] while searching for $read_name_1\n";
+	       last;
+	    }
          }
          # find read_name_1 >= read_name_2 in csfasta_1
-	 #print "Read name 2: $read_name_2\n";
          while($read_name_1 < $read_name_2) {
             $csfasta_pos[0] = get_next_read($csfasta_fhs[0], \$read_name_1);
-	    #print "Read name 1: $read_name_1\r";
+	    if ($csfasta_pos[0] == -1) {
+	       print STDERR "Warning! Reached end of $csfasta_files[0] while searching for $read_name_2\n";
+	       last;
+	    }
          }
       } while ($read_name_1 != $read_name_2);
-      # while read_name_1 != read_name_2
    }
 
    push(@{$csfasta_seek_pos[0]}, $csfasta_pos[0]);
@@ -379,14 +502,16 @@ for ($chunk = 1; $chunk < $chunks; $chunk++) {
 
    # find read_name_1 in qual_1
 
-   $qual_pos[0] = $chunk * $qual_chunk_sizes[0];   
-   $qual_pos[0] = get_matching_read($qual_fhs[0], $read_name_1, $qual_seek_pos[0], $qual_chunk_sizes[0]);
-   push(@{$qual_seek_pos[0]}, $qual_pos[0]);
+   $qual_pos[0] = $chunk * $qual_chunk_sizes[0];
+   $qual_pos[0] = get_matching_read($qual_fhs[0], $read_name_1, \$qual_read_name_1,
+				    $qual_seek_pos[0], $qual_chunk_sizes[0], $qual_file_sizes[0], 1);
 
+   push(@{$qual_seek_pos[0]}, $qual_pos[0]);
    if ($paired) {
       # find read_name_2 in qual_2
       $qual_pos[1] = $chunk * $qual_chunk_sizes[1];
-      $qual_pos[1] = get_matching_read($qual_fhs[1], $read_name_2, $qual_seek_pos[1], $qual_chunk_sizes[1]);
+      $qual_pos[1] = get_matching_read($qual_fhs[1], $read_name_1, \$qual_read_name_2,
+				       $qual_seek_pos[1], $qual_chunk_sizes[1], $qual_file_sizes[1], 1);
       push(@{$qual_seek_pos[1]}, $qual_pos[1]);
    }
 
@@ -399,6 +524,8 @@ for ($chunk = 1; $chunk < $chunks; $chunk++) {
    }
 
    push (@start_strings, $start_string);
+
+   print STDERR "$start_string\n";
 
 }
 
@@ -422,8 +549,10 @@ push (@end_strings, $end_string);
 # create jobs...
 ################
 
+my @commands = ();
 my $command;
 my $run_file;
+my $job_file;
 my $id;
 my $solid2fastq_args;
 my $compress_arg;
@@ -462,22 +591,42 @@ for my $start (@start_strings) {
    else {
       $command = $command.$csfasta_files[0]." ".$csfasta_files[1]." ".$qual_files[0]." ".$qual_files[1];
    }
-   $id = "solid2fastq.$basename.$chunk";
-   $run_file = "$RUNDIR/$id.sh";
-   my @a_sub = ();
-   $job_id = SubmitJob($run_file, $dryrun, $command, $id, 1, \@a_sub, 0);
+#   $id = "solid2fastq.$basename.$chunk";
+#   $run_file = "$RUNDIR/$id.sh";
+#   my @a_sub = ();
+#   $job_id = SubmitJob($run_file, $dryrun, $command, $id, 1, \@a_sub, 0);
+#   push (@job_ids, $job_id);
 
-   push (@job_ids, $job_id);
+   push (@commands, $command);
 }
 
-if (0 < $wait) {
-   $command = "echo 'Splitting Complete.'";
+$id = "solid2fastq.$basename";
+$run_file = "$RUNDIR/$id.sh";
+$job_file = "$RUNDIR/$id.jobs";
+my @a_sub = ();
+$job_id = SubmitArrayJob($run_file, $job_file, $dryrun, \@commands, $id, 1, \@a_sub, $wait && !$merge_single);
+push (@a_sub, $job_id);
 
-   $id = "psolid2fastq.$basename";
+# if (0 < $wait) {
+#    $command = "echo 'Splitting Complete.'";
+
+#    $id = "psolid2fastq.$basename";
+#    $run_file = "$RUNDIR/$id.sh";
+
+#    $job_id = SubmitJob($run_file, $dryrun, $command, $id, 1, \@job_ids, 1);
+# }
+
+if ($merge_single) {
+   my $ext = "fastq";
+   if ($gzip_output) {
+      $ext .= ".gz";
+   }
+   $command = "(cat $output_dir/*.single.* > $output_prefix.SINGLE.$ext && rm $output_dir/*.single.* && mv $output_prefix.SINGLE.$ext $output_prefix.single.$ext)";
+   $id = "concat.$basename";
    $run_file = "$RUNDIR/$id.sh";
-
-   $job_id = SubmitJob($run_file, $dryrun, $command, $id, 1, \@job_ids, 1);
+   $job_id = SubmitJob($run_file, $dryrun, $command, $id, 1, \@a_sub, $wait);
 }
+
 
 __END__
 
@@ -500,7 +649,8 @@ psolid2fastq.pl [options] [csfasta_files] [qual_files]
    --wait                              wait for last job to finish before exiting
    -b                                  Enable bwa output (for 'bwa aln', not for 'bfast bwaaln').
    -w                                  Create a single file to dump reads with only one end.
-   -Z                                  Output files are gzip comressed
+   -Z                                  Output files are gzip compressed
+   --merge-single                      merge single-end read files into one file (-b or -w only)
 
 Only specify one of --split-size or --chunks
 
@@ -553,6 +703,11 @@ Create a single file to dump reads with only one end (solid2fastq option)
 =item B<-Z>
 
 Output files are gzip compressed (solid2fastq option)
+
+=item B<--merge-single>
+
+bwa (-b) and single (-w) output modes produce many (potentially small) files with single 
+end reads.  This option allows them to be merged together after splitting.
 
 =back
 
